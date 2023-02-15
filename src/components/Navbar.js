ec0720a6f9e908efcb056e2aed867113fd78ea89
janeesh <janeesh.tin@gmail.com>
2023-02-15 20:13:11 +0530

    starting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-blue'>Navbar</div>
+    <div className='text-blue-600 text-center'>
+    <div>
+      <ul>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+      <li>wuu</li>
+      
+      
+      </ul>
+    </div>
+    
+    
+    </div>
   )
 }
 
